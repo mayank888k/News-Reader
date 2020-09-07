4095628ec9d51feca2499ebaa62b6f87e73aec14
Mayank <mayank963k@gmail.com>
2020-09-07 17:47:16 +0530

    News Reader

--- newsreader.py
+++ newsreader.py
@@ -1,24 +1,26 @@
-import requests
-import json
+if __name__ == "__main__":
 
-def speak(strr):
-    from win32com.client import Dispatch
-    speak=Dispatch("SAPI.SpVoice")
-    speak.speak(strr)
+    import requests
+    import json
 
-url="http://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=7d252cc8b0624836b5fc14a1cedf295c"
+    def speak(strr):
+        from win32com.client import Dispatch
+        speak=Dispatch("SAPI.SpVoice")
+        speak.speak(strr)
 
-data=requests.get(url).text
+    url="http://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=7d252cc8b0624836b5fc14a1cedf295c"
 
-dict1=json.loads(data)
+    data=requests.get(url).text
 
-speak("Todays Entertainment News of India are")
-for i,art in enumerate(dict1["articles"]):
-    print("\nHeadline No :",i+1,"\n")
-    print(art["title"])
-    speak(art["title"])
-    if i!=len(dict1["articles"])-1:
-        speak("Next headline is")
-    else:
-        speak("News Ended")
-        print("News Emded")
+    dict1=json.loads(data)
+
+    speak("Todays Entertainment News of India are")
+    for i,art in enumerate(dict1["articles"]):
+        print("\nHeadline No :",i+1,"\n")
+        print(art["title"])
+        speak(art["title"])
+        if i!=len(dict1["articles"])-1:
+            speak("Next headline is")
+        else:
+            speak("News Ended")
+            print("News Emded")
